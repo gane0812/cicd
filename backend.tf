304af3b2590b8e3747569de55795d4123e77037e
Ganesh <Ganeshsaravanan@gmail.com>
2025-02-07 19:44:54 +0000

    added backend

---
+++ backend.tf
@@ -0,0 +1,11 @@
+
+# Remote backend for state files
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storeabcd18dhr"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "cicd"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+   key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
